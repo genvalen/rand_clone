6265387d77b6b7ab1e0c9d7d5457dcf8e7f29cfe
genvalen <genvalen@protonmail.com>
2022-01-24 12:24:54 -0500

    Clean up a bit

--- CSPRNG.py
+++ CSPRNG.py
@@ -14,22 +14,33 @@ class CSPRNG:
     def update_entropy_count(self):
         self.entropy_count += 1
 
-    def mix_pool_bytes(self, random_event: str) -> None:
-        self.update_entropy_count()
+    def reset_entropy_count(self):
+        self.entropy_count = 0
 
-        random_event_numerical = str(ord(random_event))
+    def mix_pool_bytes(self, random_event: str) -> None:
+        """Mix new random events into the existing entropy pool."""
+        self.update_entropy_count()  # Track entropy introduced.
+
+        random_event_numerical = str(
+            ord(random_event)
+        )  # Represent key press numerically.
 
         new_entropy_bytes = hashlib.blake2s(
             random_event_numerical.encode("utf-8")
         ).digest()
 
-        self.entropy_pool.update(self.entropy_pool_in_bytes + new_entropy_bytes)
+        self.entropy_pool.update(new_entropy_bytes)
         self.entropy_pool_in_bytes = self.entropy_pool.digest()
 
     def get_entropy_count(self) -> int:
         return self.entropy_count
 
     def get_random_bytes(self) -> str:
-        return self.entropy_pool.digest().decode(
-            "utf-8", "ignore"
-        )  # Ignore unicode errors.
+
+        if self.entropy_count >= 16:
+            self.reset_entropy_count()
+            return self.entropy_pool.digest().decode(
+                "utf-8", "ignore"
+            )  # Ignore unicode errors.
+
+        return " "
--- random.py
+++ random.py
@@ -7,20 +7,14 @@ from sshkeyboard import listen_keyboard
 csprng = CSPRNG()
 
 # Collect random user input from the keyboard.
-# Mix, or stir, every key pressed into the existing entropy pool of CSPRNG.
+# Mix, or stir, every key press into the existing entropy pool of CSPRNG.
 def press(key):
     csprng.mix_pool_bytes(random_event=key)
 
 
-def release(key):
-    print(" ")  # user will see nothing whiles keys are pressed
+# Produce output only while entropy pool has the min required new entropy.
+while True:
+    listen_keyboard(on_press=press, until="enter")
 
-
-# Produce output only while entropy pool has the min required new entropy
-while csprng.entropy_count < csprng.MIN_ENTROPY:
-    print("Type in some more random chars: ")
-    listen_keyboard(on_press=press, on_release=release, until="enter")
-
-# print("Random output:")
-with open("/dev/tty", "a") as f:
-    f.write(csprng.get_random_bytes())
+    with open("/dev/tty", "a") as f:
+        f.write(csprng.get_random_bytes())
