76c114950ba6c74d9c7173071bfa3d6e5659bc94
genvalen <genvalen@protonmail.com>
2022-01-24 20:27:51 -0500

    Add documentation

--- CSPRNG.py
+++ CSPRNG.py
@@ -4,43 +4,79 @@ import hashlib
 class CSPRNG:
     def __init__(
         self,
-        seed: str = "00000000",
+        base_entropy: str = "00000000"
+        * 64,  # Simulates 512 bytes / 4096 bits of deterministic entropy
     ) -> None:
-        self.MIN_ENTROPY = 16  # aka 128 bits
-        self.entropy_pool = hashlib.blake2s(seed.encode("utf-8"))
-        self.entropy_pool_in_bytes = self.entropy_pool.digest()
+        self.MIN_ENTROPY = 16  # /dev/random reads 128 bytes at a time -- 16 is for testing
+        self.entropy_pool = hashlib.sha1(base_entropy.encode("utf-8"))
+        self.entropy_pool_in_bytes = self.entropy_pool.digest()  # byte version of pool
         self.entropy_count = 0
 
-    def update_entropy_count(self):
+    def update_entropy_count(self) -> int:
+        "Increment number of bytes available for use by 1."
         self.entropy_count += 1
 
-    def reset_entropy_count(self):
+    def reset_entropy_count(self) -> None:
+        """Reset self.entropy_count to `0`.
+
+        This only gets called if `get_random_bytes` has collected sufficient
+        entropy to print a random bytes output on the console.
+        """
         self.entropy_count = 0
 
     def mix_pool_bytes(self, random_event: str) -> None:
-        """Mix new random events into the existing entropy pool."""
-        self.update_entropy_count()  # Track entropy introduced.
+        """Mix random event (user input) into the existing entropy pool.
 
-        random_event_numerical = str(
-            ord(random_event)
-        )  # Represent key press numerically.
+        Random events are passed in as a single char, converted to an
+        integer using Python's ord() fucntion, serialized using Sha1, and
+        then mixed into the existing entropy pool. Sha1 is not crytographially
+        secure, but is reasonable and fast enough to use during each interrupt.
 
-        new_entropy_bytes = hashlib.blake2s(
+        Each time this method is called, `update_entropy_count` is called, too,
+        to keep track of how much new entropy is collected.
+        """
+        self.update_entropy_count()  # Track entropy.
+
+        try:
+            random_event_numerical = str(ord(random_event))
+        except:
+            # This is needed because, occasionally,
+            # sshkeyboard passes in more than one char at a time, causing an error.
+            random_event_numerical = "0"
+
+        new_entropy_bytes = hashlib.sha1(
             random_event_numerical.encode("utf-8")
         ).digest()
-
-        self.entropy_pool.update(new_entropy_bytes)
+        self.entropy_pool = hashlib.sha1(self.entropy_pool_in_bytes + new_entropy_bytes)
         self.entropy_pool_in_bytes = self.entropy_pool.digest()
 
     def get_entropy_count(self) -> int:
+        """Return amount of new entropy seen since last random bytes output."""
         return self.entropy_count
 
     def get_random_bytes(self) -> str:
-
+        """Take the blake2s hash of the entropy pool and print the random ouput
+        to the console *IF* 16 bytes or more of new entropy have been collected
+        since the last output dump. Otherwise, block -- simulated by returning an
+        empty string -- until more entropy is collected.
+        """
         if self.entropy_count >= 16:
             self.reset_entropy_count()
-            return self.entropy_pool.digest().decode(
-                "utf-8", "ignore"
-            )  # Ignore unicode errors.
+            blake2s_hash = hashlib.blake2s(self.entropy_pool_in_bytes)
 
+            # Mix the output back into the pool to prevent backtracking
+            # attacks (where the attacker knows the state of the pool
+            # plus the current outputs and attempts to find previous
+            # outputs).
+            self.entropy_pool = hashlib.sha1(
+                self.entropy_pool_in_bytes + blake2s_hash.digest()
+            )
+            self.entropy_pool_in_bytes = self.entropy_pool.digest()
+
+            # Return random output.
+            return blake2s_hash.digest().decode(
+                "utf-8", "ignore"  # Ignore unicode errors.
+            )
+
+        # Simulates blocking. 
         return " "
--- random.py
+++ random.py
@@ -1,18 +1,25 @@
 """Clone /dev/random, a CSPRNG that makes use of random real-world events to create
-a hash that is impossible to reverse using brute force.
+a hash that is impossible to reverse through brute force or back tracking.
 """
 from CSPRNG import CSPRNG
 from sshkeyboard import listen_keyboard
 
+# Instantiate a CSPRNG.
 csprng = CSPRNG()
 
-# Collect random user input from the keyboard.
-# Mix, or stir, every key press into the existing entropy pool of CSPRNG.
+# Mix, or stir, each key press into the entropy pool of the CSPRNG object.
 def press(key):
     csprng.mix_pool_bytes(random_event=key)
 
 
-# Produce output only while entropy pool has the min required new entropy.
+# Main program -->
+# Manual labor is required here:
+#   Press random keys on keyboard for 2-3 seconds, then press enter.
+#   Repeat this process a few times.
+#   If enough entropy has been collected while pressing keys (>= 16 bytes), then
+#       random output will be printed to the console when "enter" is pressed.
+#       Otherwise, no output is printed when "enter" is pressed.
+#   Press "CTR-C" to exit the program.
 while True:
     listen_keyboard(on_press=press, until="enter")
 
